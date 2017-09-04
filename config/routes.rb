780e7fcbab375f0ab5965bbb2e7568d2cf818f6b
sam hinton <sam.hinton88@gmail.com>
2017-09-04 14:04:39 +0200

    longest word game

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  get 'game', to: 'wordgame#game'
+
+  get 'score', to: 'wordgame#score'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
